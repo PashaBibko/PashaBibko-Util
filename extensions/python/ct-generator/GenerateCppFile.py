5d1ccf0736674da5195dedf9f9a299752d50abfc
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-08-16 00:16:41 +0100

    Added type filtering to the py dict

--- extensions/python/ct-generator/GenerateCppFile.py
+++ extensions/python/ct-generator/GenerateCppFile.py
@@ -1,5 +1,25 @@
+# Fetches data stored by C API #
+values = globals().get('values')
 
-print("Saved data:")
+# Types that can be written to the CPP file #
+allowed_types = (int, float, str)
 
-for k, v in values.items():
-        print(f"    {k} = {v}")
+# Dictionary to store filtered values by type #
+type_dicts = { t.__name__: {} for t in allowed_types }
+
+for key, value in values.items():
+    # Skip 'hidden' items #
+    if key[:2] == "__":
+        continue
+
+    # Skip invalid types #
+    if not isinstance(value, allowed_types):
+        print(f"Invalid type: {type(value).__name__} for key '{key}'")
+
+    # Adds value to correct dictionary #
+    type_name = type(value).__name__
+    type_dicts[type_name][key] = value
+
+# Print results (temporary) #
+for t, d in type_dicts.items():
+    print(f"{t}: {d}")
