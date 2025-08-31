ef70348321680b53131ff5d5914e7c87ccb428ec
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-08-31 14:59:09 +0100

    Added equality and constructor tests

--- code-tests/src/VecTests.cpp
+++ code-tests/src/VecTests.cpp
@@ -4,6 +4,24 @@ PB_TEST_GROUP(PBU_VectorTests);
 
 namespace PashaBibko::Util::CodeTests
 {
+    PB_TEST(PBU_VectorTests, VecConstructors)
+    {
+        /* Default constructor */
+        Vec2i a;
+        PB_EXPECT_EQL(a.x, 0);
+        PB_EXPECT_EQL(a.y, 0);
+
+        /* Single item constructor */
+        Vec2i b(56);
+        PB_EXPECT_EQL(b.x, 56);
+        PB_EXPECT_EQL(b.y, 56);
+
+        /* Multi-item constructor */
+        Vec2i c(23117, 333);
+        PB_EXPECT_EQL(c.x, 23117);
+        PB_EXPECT_EQL(c.y, 333);
+    }
+
     PB_TEST(PBU_VectorTests, Vec2_Aliases)
     {
         Vec2f v(4.0f);
@@ -77,5 +95,56 @@ namespace PashaBibko::Util::CodeTests
         PB_EXPECT_EQL(v.g, 534);
         PB_EXPECT_EQL(v.b, 434);
         PB_EXPECT_EQL(v.a, 503);
+    }
+
+    PB_TEST(PBU_VectorTests, VecIterator)
+    {
+        /* Non - const iterator */
+        Util::Vec<10, int> vecA(0, 1, 2, 3, 45, 6, 7, 8, 9, 11);
+        int arrA[] = { 0, 1, 2, 3, 45, 6, 7, 8, 9, 11 };
+
+        std::size_t indexA = 0;
+        for (int& item : vecA)
+        {
+            PB_EXPECT_EQL(item, arrA[indexA]);
+            indexA++;
+        }
+
+        /* Const iterator */
+        Util::Vec<10, int> vecB(6, 7, 2, 3, 1, 1, 7, 4, 2, 0);
+        int arrB[] = { 6, 7, 2, 3, 1, 1, 7, 4, 2, 0 };
+
+        std::size_t indexB = 0;
+        for (const int& item : vecB)
+        {
+            PB_EXPECT_EQL(item, arrB[indexB]);
+            indexB++;
+        }
+    }
+
+    PB_TEST(PBU_VectorTests, VecEql)
+    {
+        Vec4i a(1, 2, 3, 4);
+        Vec4i b(5, 6, 7, 8);
+        Vec4i c(1, 2, 3, 4);
+        Vec4i d(5, 6, 9, 8);
+
+        PB_EXPECT_FALSE(a == b);
+        PB_EXPECT_FALSE(b == d);
+
+        PB_EXPECT_TRUE(a == c);
+    }
+
+    PB_TEST(PBU_VectorTests, VecNEql)
+    {
+        Vec4i a(1, 2, 3, 4);
+        Vec4i b(1, 2, 5, 4);
+        Vec4i c(5, 6, 7, 8);
+        Vec4i d(5, 6, 7, 8);
+
+        PB_EXPECT_TRUE(a != b);
+        PB_EXPECT_TRUE(a != c);
+
+        PB_EXPECT_FALSE(c != d);
     }
 };
